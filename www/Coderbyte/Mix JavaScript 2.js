ff12e16bdd274e5d4f67589ff4b36a868345b03a
pablopicouceira <pablo.pico.uceira@gmail.com>
2019-11-23 10:25:10 +0100

    solucionado 'Counting Minutes'

--- www/Coderbyte/Mix JavaScript 2.js
+++ www/Coderbyte/Mix JavaScript 2.js
@@ -2,16 +2,16 @@
 
 // Consecutive
 
-// Have the function Consecutive(arr) take the array of integers stored in arr and return the minimum number of integers needed to make the
-// contents of arr consecutive from the lowest number to the highest number.For example: If arr contains[4, 8, 6] then the output should be
-// 2 because two numbers need to be added to the array(5 and 7) to make it a consecutive array of numbers from 4 to 8. Negative numbers may
-// be entered as parameters and no array will have less than 2 elements.
+// Have the function Consecutive(arr) take the array of integers stored in arr and return the minimum number of integers needed to make the contents of arr
+// consecutive from the lowest number to the highest number.For example: If arr contains[4, 8, 6] then the output should be 2 because two numbers need to be
+// added to the array(5 and 7) to make it a consecutive array of numbers from 4 to 8. Negative numbers may be entered as parameters and no array will have less
+// than 2 elements.
 
 /* Ordenamos el array para conseguir el valor más alto y el más bajo. Después usamos un bucle while con estos dos valores que usamos para 
    para pushear los valores intermedios a un nuevo array. El resultado saldŕa de restarle a la longitud de ese nuevo array la longitud del
    array original - 2 (se le resta - 2 porque así nos queda la longitud de los elementos intermedios, que deberíamos eliminar del array 
    nuevo) */
-
+/*
 let array = [-2, 10, 4];
 
 function Consecutive(arr) {
@@ -37,7 +37,7 @@ console.log(Consecutive(array));
 // For example: if num is 910, the output should be 1 because 910 can be arranged into 109 or 019, both of which are primes.
 
 /* El ejercicio no lo dice, pero el número será < 1000. */
-
+/*
 let number = 45; //465, 546, 564, 645, 654
 
 console.log(number.toString().length);
@@ -106,21 +106,68 @@ var grupo = 4; //le indico la cantidad de cuantos caracteres quiero que sean las
 comb(cadena, grupo, arrayCombinaciones);
 
 console.log(JSON.stringify(arrayCombinaciones));
-
+*/
 // Counting Minutes
 
 // Have the function CountingMinutes(str) take the str parameter being passed which will be two times (each properly formatted with a colon
 // and am or pm) separated by a hyphen and return the total number of minutes between the two times.The time will be in a 12 hour clock
 // format.For example: if str is 9: 00am - 10: 00am then the output should be 60. If str is 1: 00pm - 11: 00am the output should be 1320.
 
-let input = "2:30pm-1:00am";
-console.log(input.split(""));
+let input = "1:00pm-11:00am";
 
-let start = input.slice(0, input.indexOf("-"));
-console.log(start);
+function CountingMinutes(str) {
+  let start = str.slice(0, str.indexOf("-"));
+  let startAmPm = start.slice(start.length - 2);
+  let startHour = parseInt(start.slice(0, start.indexOf(":")));
 
-let finish = input.slice(input.indexOf("-") + 1);
-console.log(finish);
+  if (startAmPm == "am") {
+    startHour = parseInt(start.slice(0, start.indexOf(":")));
+  } else {
+    parseInt(start.slice(0, start.indexOf(":")));
+  }
+
+  let startMinutes = parseInt(
+    start.slice(start.indexOf(":") + 1, start.length - 2)
+  );
+
+  let totalStartMinutes = startHour * 60 + startMinutes;
+  let finish = str.slice(str.indexOf("-") + 1);
+  let finishAmPm = finish.slice(finish.length - 2);
+  let finishHour = parseInt(finish.slice(0, finish.indexOf(":")));
+  console.log(totalStartMinutes);
+
+  if (finishAmPm == "am") {
+    finishHour = parseInt(finish.slice(0, finish.indexOf(":")));
+  } else {
+    parseInt(finish.slice(0, finish.indexOf(":")));
+  }
+
+  let finishMinutes = parseInt(
+    finish.slice(finish.indexOf(":") + 1, finish.length - 2)
+  );
+
+  let totalFinishMinutes = finishHour * 60 + finishMinutes;
+  console.log(totalFinishMinutes);
+  let result;
+
+  console.log(totalFinishMinutes - totalStartMinutes);
+
+  if (startAmPm === finishAmPm) {
+    if (totalStartMinutes < totalFinishMinutes) {
+      result = totalFinishMinutes - totalStartMinutes;
+    } else {
+      result = 1440 - Math.abs(totalFinishMinutes - totalStartMinutes);
+    }
+  } else {
+    if (totalStartMinutes < totalFinishMinutes) {
+      result = 720 - totalStartMinutes + totalFinishMinutes; //1320
+    } else {
+      result = 720 - (totalStartMinutes - totalFinishMinutes);
+    }
+  }
+  return result;
+}
+console.log(CountingMinutes(input));
 
 // Formatted Division
 
@@ -128,22 +175,15 @@ console.log(finish);
 // formatted commas and 4 significant digits after the decimal place.For example: if num1 is 123456789 and num2 is 10000 the output should be "12,345.6789". The
 // output must contain a number in the one's place even if it is a zero.
 
-// console.log(result);
-
-// console.log(stringWith4Decimals);
-
-// console.log(stringWith4Decimals.length);
-// let otro = result;
-
-// console.log(newArray);
-
-// console.log(solution);
+/* En primer lugar, obtenemos un string con 4 decimales. Despueś extraemos a un array los decimales incluido el punto, y a otro la parte entera, que es la que 
+   formatearemo */
+/*
 let input1 = 454879795;
 let input2 = 3;
 
 function FormattedDivision(num1, num2) {
   let result = num1 / num2;
-  let stringWith4Decimals = result.toFixed(4).toString();
+  let stringWith4Decimals = result.toFixed(4).toString(); // Aquí obtenemos el string con 4 decimales
   let arrayWith4Decimals = [];
 
   for (let i = 0; i < stringWith4Decimals.length; i++) {
@@ -153,17 +193,7 @@ function FormattedDivision(num1, num2) {
   let leftArray = arrayWith4Decimals.slice(0, arrayWith4Decimals.indexOf("."));
   let rightArray = arrayWith4Decimals.slice(arrayWith4Decimals.indexOf("."));
   let resto = leftArray.length % 3;
-  console.log(leftArray);
-  console.log(rightArray);
-  console.log(leftArray.length);
-  console.log(resto);
-  // let solution2 = leftArray.split("");
 
-  // solution2.splice(resto, 0, ",");
-  console.log(leftArray);
-  console.log(`Resto: ${resto}`);
-  // newString.split("");
-  // console.log(newString);
   if (resto !== 0) {
     for (let i = 0; i < leftArray.length - 2; i++) {
       console.log(i);
@@ -173,15 +203,97 @@ function FormattedDivision(num1, num2) {
     }
   } else {
     for (let i = 3; i < leftArray.length - 2; i++) {
-      console.log(i);
       leftArray.splice(resto + i, 0, ",");
-      console.log(leftArray);
+
       i = i + 3;
     }
-
-    //solution2.splice(0, 1);
   }
   return leftArray.concat(rightArray).join("");
 }
 
 console.log(FormattedDivision(input1, input2));
+*/
+
+// Counting Minutes
+
+// Have the function CountingMinutes(str) take the str parameter being passed which will be two times (each properly formatted with a colon
+// and am or pm) separated by a hyphen and return the total number of minutes between the two times.The time will be in a 12 hour clock
+// format.For example: if str is 9: 00am - 10: 00am then the output should be 60. If str is 1: 00pm - 11: 00am the output should be 1320.
+
+// let input = "1:00pm-11:00am";
+
+// function CountingMinutes(str) {
+//   console.log(str.split(""));
+
+//   let start = str.slice(0, str.indexOf("-"));
+//   console.log(start);
+
+//   let startAmPm = start.slice(start.length - 2);
+//   console.log(startAmPm);
+
+//   let startHour = parseInt(start.slice(0, start.indexOf(":"))) + 12;
+//   console.log(startHour);
+
+//   if (startAmPm == "am") {
+//     startHour = parseInt(start.slice(0, start.indexOf(":")));
+//   } else {
+//     parseInt(start.slice(0, start.indexOf(":"))) + 12;
+//   }
+
+//   let startMinutes = parseInt(
+//     start.slice(start.indexOf(":") + 1, start.length - 2)
+//   );
+//   console.log(startMinutes);
+
+//   console.log(startHour);
+
+//   console.log(`${startHour}:${startMinutes}`);
+
+//   let totalStartMinutes = startHour * 60 + startMinutes;
+//   console.log(totalStartMinutes);
+
+//   let finish = str.slice(str.indexOf("-") + 1);
+//   console.log(finish);
+
+//   let finishAmPm = finish.slice(finish.length - 2);
+//   console.log(finishAmPm);
+
+//   let finishHour = parseInt(finish.slice(0, finish.indexOf(":")));
+//   console.log(finishHour);
+
+//   if (finishAmPm == "am") {
+//     finishHour = parseInt(finish.slice(0, finish.indexOf(":")));
+//   } else {
+//     parseInt(finish.slice(0, finish.indexOf(":")));
+//   }
+
+//   let finishMinutes = parseInt(
+//     finish.slice(finish.indexOf(":") + 1, finish.length - 2)
+//   );
+//   console.log(finishMinutes);
+
+//   console.log(finishHour);
+
+//   console.log(`${finishHour}:${finishMinutes}`);
+
+//   let totalFinishMinutes = finishHour * 60 + finishMinutes;
+//   console.log(totalFinishMinutes);
+
+//   let result;
+
+//   if (startAmPm === finishAmPm) {
+//     if (totalStartMinutes < totalFinishMinutes) {
+//       result = totalFinishMinutes - totalStartMinutes;
+//     } else {
+//       result = 1440 - Math.abs(totalFinishMinutes - totalStartMinutes);
+//     }
+//   } else {
+//     if (totalStartMinutes < totalFinishMinutes) {
+//       result = 1440 + totalFinishMinutes - totalStartMinutes;
+//     } else {
+//       result = 1440 - Math.abs(totalFinishMinutes - totalStartMinutes);
+//     }
+//   }
+//   return result;
+// }
+// console.log(CountingMinutes(input));
