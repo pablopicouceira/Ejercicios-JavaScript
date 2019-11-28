ab385297caaedc3b41f0a7c0d3191f84ce2d7df2
pablopicouceira <pablo.pico.uceira@gmail.com>
2019-11-28 14:20:05 +0100

    solucionado 'Number Search'

--- www/Coderbyte/Mix JavaScript 2.js
+++ www/Coderbyte/Mix JavaScript 2.js
@@ -465,3 +465,40 @@ function isALetter(caracter) {
   let ascii = caracter.toUpperCase().charCodeAt(0);
   return ascii > 64 && ascii < 91;
 }
+
+// Number Search
+
+// Have the function NumberSearch(str) take the str parameter, search for all the numbers in the string, add them together, then return that
+// final number divided by the total amount of letters in the string.For example: if str is "Hello6 9World 2, Nic8e D7ay!" the output should
+// be 2. First if you add up all the numbers, 6 + 9 + 2 + 8 + 7 you get 32. Then there are 17 letters in the string. 32 / 17 = 1.882, and the
+// final answer should be rounded to the nearest whole number, so the answer is 2. Only single digit numbers separated by spaces will be used
+// throughout the whole string(So this won't ever be the case: hello44444 world). Each string will also have at least one letter.
+
+let string1 = "Hello6 9World 2, Nic8e D7ay!";
+
+function NumberSearch(str) {
+  let numbers = [];
+  let letters = [];
+  for (let i = 0; i < str.length; i++) {
+    if (isANumber(str[i])) {
+      numbers.push(parseInt(str[i]));
+    } else if (isALetter(str[i])) {
+      letters.push(str[i]);
+    }
+  }
+
+  const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+  let numbersValue;
+  if (numbers.length === 0) {
+    numbersValue = 0;
+  } else numbersValue = numbers.reduce(reducer);
+
+  let lettersValue = letters.length;
+
+  let result = Math.round(numbersValue / lettersValue);
+
+  return result;
+}
+
+console.log(NumberSearch(string1));
